5178d1b51475016979735fb8f5ae6771544e49d5
Trey <qwestionmark90@gmail.com>
2020-08-23 12:29:30 -0400

    Initialized project repo with express boilerplate.

---
+++ start.js
@@ -0,0 +1,29 @@
+// Credit to Robin Wieruch's excellent Graphql tutorial for the boilerplate walkthrough
+// Tutorial: https://www.robinwieruch.de/graphql-apollo-server-tutorial
+
+// NOTE: KEEP DOTENV AS FIRST IMPORT :D
+import 'dotenv/config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import models from './src/models/index';
+import schema from './src/schema/index';
+import resolvers from './src/resolvers/index';
+
+const app = express();
+
+// Deconstruct ENV vars for brevity
+const { ROOT_ENDPOINT, PORT } = process.env
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+  context: {
+      models,
+  }
+});
+
+server.applyMiddleware({ app, path: ROOT_ENDPOINT });
+
+app.listen({ port: PORT }, () => {
+    console.log(`Server is running at http://localhost:${PORT}${ROOT_ENDPOINT}`);
+});
