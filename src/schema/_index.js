2208aee7d91671fff06e80095d6de14e4413bd3a
Trey <qwestionmark90@gmail.com>
2020-08-31 09:17:27 -0400

    Created resolvers for 5e-database queries.

--- src/schema/_index.js
+++ src/schema/_index.js
@@ -1,25 +1,25 @@
 import { gql } from 'apollo-server-express';
-// import abilityScores from './ability-scores';
-// import classes from './classes';
-// import conditions from './conditions';
-// import damageTypes from './damage-types';
-// import equipment from './equipment';
-// import equipmentCategories from './equipment-categories';
-// import features from './features';
-// import languages from './languages';
-// import levels from './levels';
-// import magicSchools from './magic-schools';
-// import monsters from './monsters';
-// import proficiencies from './proficiencies';
-// import races from './races';
-// import skills from './skills';
+import abilityScores from './ability-scores';
+import classes from './classes';
+import conditions from './conditions';
+import damageTypes from './damage-types';
+import equipment from './equipment';
+import equipmentCategories from './equipment-categories';
+import features from './features';
+import languages from './languages';
+import levels from './levels';
+import magicSchools from './magic-schools';
+import monsters from './monsters';
+import proficiencies from './proficiencies';
+import races from './races';
+import skills from './skills';
 import spells from './spells';
-// import spellcasting from './spellcasting';
-// import startingEquipment from './starting-equipment';
-// import subclasses from './subclasses';
-// import subraces from './subraces';
-// import traits from './traits';
-// import weaponProperties from './weapon-properties';
+import spellcasting from './spellcasting';
+import startingEquipment from './starting-equipment';
+import subclasses from './subclasses';
+import subraces from './subraces';
+import traits from './traits';
+import weaponProperties from './weapon-properties';
  
 const linkSchema = gql`
   type Query {
@@ -36,8 +36,6 @@ const linkSchema = gql`
   }
 `;
  
-// export default [linkSchema, abilityScores, classes, conditions, damageTypes, equipment, equipmentCategories, features, 
-// languages, levels, magicSchools, monsters, proficiencies, races, skills, spells, spellcasting, startingEquipment,
-// subclasses, subraces, traits, weaponProperties];
-
-export default [linkSchema, spells]
+export default [linkSchema, abilityScores, classes, conditions, damageTypes, equipment, equipmentCategories, features, 
+languages, levels, magicSchools, monsters, proficiencies, races, skills, spells, spellcasting, startingEquipment,
+subclasses, subraces, traits, weaponProperties];
